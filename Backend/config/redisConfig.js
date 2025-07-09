ddf4353dca1900496eba375b351659199b18933a
ali-md321 <mohammadaliilur@gmail.com>
2025-07-09 14:45:39 +0530

    fix:updated Redis Upstash

--- Backend/config/redisConfig.js
+++ Backend/config/redisConfig.js
@@ -1,3 +1,6 @@
-const Redis = require("ioredis");
-redisClient = new Redis();
+import { Redis } from '@upstash/redis'
+const redis = new Redis({
+  url: 'https://master-cougar-56784.upstash.io',
+  token: process.env.REDIS_URL,
+})
 module.exports = redisClient;
