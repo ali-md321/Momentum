c6e4e0d54505cd6b3488c98b0c332b33a35955a3
ali-md321 <mohammadaliilur@gmail.com>
2025-07-09 14:50:33 +0530

    fix:updated Redis Env Error

--- Backend/config/redisConfig.js
+++ Backend/config/redisConfig.js
@@ -1,6 +1,6 @@
 import { Redis } from '@upstash/redis'
 const redisClient = new Redis({
   url: 'https://master-cougar-56784.upstash.io',
-  token: process.env.REDIS_URL,
+  token: process.env.REDIS_TOKEN,
 })
 module.exports = redisClient;
