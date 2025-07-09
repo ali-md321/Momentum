997610072569327f26b367fae6364306e4e3314e
ali-md321 <mohammadaliilur@gmail.com>
2025-07-09 17:27:27 +0530

    Fix in AxiosInstance

--- Frontend/src/utils/axiosInstance.js
+++ Frontend/src/utils/axiosInstance.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const baseURL =
   import.meta.env.MODE === "development"
     ? "/api"
-    : "https://momentum-s326.onrender.com/api";
+    : "https://momentum-s326.onrender.com/";
 
 export const axiosInstance = axios.create({
   baseURL,
