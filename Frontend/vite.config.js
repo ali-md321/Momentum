87a67c54d449fd6266fb421acdd2372d17f8dfc4
ali-md321 <mohammadaliilur@gmail.com>
2025-07-09 12:26:12 +0530

    Changed Backend live server

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -20,7 +20,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://momentum-s326.onrender.com' /*'http://localhost:3000'*/,
         changeOrigin: true,
         secure: false
       }
