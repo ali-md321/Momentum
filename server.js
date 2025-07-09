f8f05e9748227668ab37a9682a5a517449650143
ali-md321 <mohammadaliilur@gmail.com>
2025-07-09 15:04:28 +0530

    fix:require dotenv

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-require('dotenv').config(); 
+if(process.env.NODE_ENV !== "production"){
+  require('dotenv').config(); 
+}
 const express = require("express");
 const app = require("./Backend/app");
 const path = require("path");
